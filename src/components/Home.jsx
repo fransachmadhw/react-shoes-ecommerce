1d9266ff3d4a23ceaef9a800a058252a3864e451
frans <franswinata6@gmail.com>
2022-11-26 18:19:57 +0700

    dataSepatu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,30 +5,36 @@ import shoes1 from '../assets/shoes1.jpg'
 import shoes2 from '../assets/shoes2.jpg'
 import shoes3 from '../assets/shoes3.jpg'
 import shoes4 from '../assets/shoes4.jpg'
+import { data } from '../dataSepatu'
 
 const Home = () => {
-    const dataSepatu = {
-        product1: {
-            title: 'Gearfourth Sneakers Blunt Os Black White',
-            price: '115.000',
-            image: shoes1,
-        },
-        product2: {
-            title: 'Gearfourth Sneakers Willy Os Black Maroon',
-            price: '99.000',
-            image: shoes2,
-        },
-        product3: {
-            title: 'Gearfourth Sneakers Goofy Os Yellow',
-            price: '99.000',
-            image: shoes3,
-        },
-        product4: {
-            title: 'Gearfourth Sneakers Grind Os Black Nevy',
-            price: '99.000',
-            image: shoes4,
-        },
-    }
+    const [dataSepatu, setDataSepatu] =  React.useState(data)
+
+    // React.useEffect(() => {
+    //     setDataSepatu(e => data)
+    // }, [dataSepatu])
+    // const dataSepatu = {
+    //     product1: {
+    //         title: 'Gearfourth Sneakers Blunt Os Black White',
+    //         price: '115.000',
+    //         image: shoes1,
+    //     },
+    //     product2: {
+    //         title: 'Gearfourth Sneakers Willy Os Black Maroon',
+    //         price: '99.000',
+    //         image: shoes2,
+    //     },
+    //     product3: {
+    //         title: 'Gearfourth Sneakers Goofy Os Yellow',
+    //         price: '99.000',
+    //         image: shoes3,
+    //     },
+    //     product4: {
+    //         title: 'Gearfourth Sneakers Grind Os Black Nevy',
+    //         price: '99.000',
+    //         image: shoes4,
+    //     },
+    // }
 
   return (
     <div className='w-full'>
